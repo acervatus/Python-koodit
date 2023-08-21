dfe0ec3d853dfc002b017274f70d519e36261049
acervatus <142780750+acervatus@users.noreply.github.com>
2023-08-21 11:17:27 +0300

    t2-3 tehty

---
+++ t2-3.py
@@ -0,0 +1,3 @@
+kanta = float(input('Suorakulmion kanta: '))
+korkeus = float(input('Suorakulmion korkeus: '))
+print("Suorakulmion piiri: " + str(kanta * 2 + korkeus * 2) + "\n\r" + "Suorakolmion pinta-ala: " + str(kanta * korkeus))
