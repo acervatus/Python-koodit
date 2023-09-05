b1f6fcf0b36de6f7f57d1c41f8ba4bc2255249f0
acervatus <142780750+acervatus@users.noreply.github.com>
2023-09-05 12:07:24 +0300

    Tehtävä

---
+++ t7-1.py
@@ -0,0 +1,3 @@
+kuukaudet = ["talvi", "talvi", "kevät", "kevät", "kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi"]
+kuukausi = int(input("Anna kuukausinummero: "))
+print(f"Kuukausi {kuukausi} on {kuukaudet[kuukausi - 1]}")
