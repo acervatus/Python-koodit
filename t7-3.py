c50f257eb320a40692e0a62298bedd46fa189e70
acervatus <142780750+acervatus@users.noreply.github.com>
2023-09-11 11:18:13 +0300

    t

--- t7-3.py
+++ t7-3.py
@@ -1,2 +1,12 @@
-
+lentoasema = {"EFHK":"Helsinki-Vantaan lentoasema"}
 while True:
+    valinta = input("Uusi lentoasema, hae lentoasema tai lopeta(u/h/l): ")
+    if valinta == "u":
+        koodi = input("Anna lentoaseman ICAO-koodi: ")
+        nimi = input("Anna lentoaseman nimi: ")
+        lentoasema[koodi] = nimi
+    elif valinta == "h":
+        koodi = input("Anna lentoaseman ICAO-koodi: ")
+        print(lentoasema[koodi])
+    elif valinta == "l":
+        break
